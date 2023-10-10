cd4dff01fa2b8ea9e17b6f6d8ce44611b41f5c5e
Ysaito-prot <yu22.program@gmail.com>
2023-10-10 18:51:34 +0900

    課題2「インデント機能」完了

--- javascript/gannt_practice.js
+++ javascript/gannt_practice.js
@@ -1,8 +1,15 @@
 $(function () {
+  // クリックした行の「作業名」要素を格納
+  let selectedItem = "";
+
   // 課題1「複数選択行の入れ替え(ライブラリの利用)」
   $("#column_table_body")
     .selectable({
       cancel: ".ui-selected",
+      selected: function (e, ui) {
+        // 選択中の行の「作業名」要素を格納
+        selectedItem = $(".ui-selected").children("td:nth-child(2)");
+      },
     })
     .sortable({
       axis: "y",
@@ -17,9 +24,26 @@ $(function () {
         ui.item.after(selected);
         ui.item.remove();
         // 選択色解除
-        selected.removeClass('ui-selected');
-	      $(selected).children("td").removeClass('ui-selected');
+        selected.removeClass("ui-selected");
+        $(selected).children("td").removeClass("ui-selected");
       },
     });
   // 課題2「インデント機能」
+  // インデント幅の定義
+  const indentW = 12;
+  // →ボタンの処理
+  $("#indent_right").on("click", function () {
+    if (selectedItem != "") {
+      let indent = parseInt($(selectedItem).css("text-indent")) + indentW;
+      if (indent <= indentW * 3)
+        $(selectedItem).css("text-indent", indent + "px");
+    }
+  });
+  // ←ボタンの処理
+  $("#indent_left").on("click", function () {
+    if (selectedItem != "") {
+      let indent = parseInt($(selectedItem).css("text-indent")) - indentW;
+      if (0 <= indent) $(selectedItem).css("text-indent", indent + "px");
+    }
+  });
 });
